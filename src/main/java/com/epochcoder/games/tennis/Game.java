31da9180c16766ce0dc428a1a5a2ae65b5239e06
Willie Scholtz <williescholtz@gmail.com>
2020-02-23 15:41:02 +0100

    Filter for duplicate & mirrored matches

--- src/main/java/com/epochcoder/games/tennis/Game.java
+++ src/main/java/com/epochcoder/games/tennis/Game.java
@@ -1,6 +1,8 @@
 package com.epochcoder.games.tennis;
 
 import com.google.common.base.Strings;
+import com.google.common.collect.ArrayListMultimap;
+import com.google.common.collect.Multimap;
 
 import java.util.Collections;
 import java.util.HashSet;
@@ -12,13 +14,13 @@ import java.util.stream.Collectors;
 public class Game {
 
     public static void main(final String[] args) {
-        final List<Player> guys = Player.toPlayers(Gender.MALE, "willie", "gabriel", "giovanni", "thiago");
-//        final List<Player> guys = Player.toPlayers(Gender.MALE, "willie", "gabriel", "giovanni", "thiago", "peter", "ernst");
+//        final List<Player> guys = Player.toPlayers(Gender.MALE, "willie", "gabriel", "giovanni", "thiago");
+        final List<Player> guys = Player.toPlayers(Gender.MALE, "willie", "gabriel", "giovanni", "thiago", "peter", "ernst");
 //        final List<Player> girls = Player.toPlayers(Gender.FEMALE, "katrin", "sylvia", "elizibetha", "paula");
-//        final List<Player> girls = Player.toPlayers(Gender.FEMALE, "katrin", "sylvia", "elizibetha", "paula", "lilly", "laura");
+        final List<Player> girls = Player.toPlayers(Gender.FEMALE, "katrin", "sylvia", "elizibetha", "paula", "lilly", "laura");
 
 //        final List<Player> guys = Player.toPlayers(Gender.MALE, "1", "2", "3", "4");
-        final List<Player> girls = Player.toPlayers(Gender.FEMALE, "A", "B", "C", "D");
+//        final List<Player> girls = Player.toPlayers(Gender.FEMALE, "A", "B", "C", "D");
 
 //        final List<Player> guys = Player.toPlayers(Gender.MALE, "willie", "gabe");
 //        final List<Player> girls = Player.toPlayers(Gender.FEMALE, "katrin", "syl");
@@ -47,56 +49,76 @@ public class Game {
             System.out.println("v3 Match Maker init!");
             System.out.println("-==================-");
 
-            final Set<Set<Match>> sets = findPossibleMatches(
+            final Set<Set<Match>> foundSets = findMatchSets(
                     Collections.unmodifiableSet(teams),
                     Collections.unmodifiableSet(allPlayers),
                     new HashSet<>(0), 0);
 
-            System.out.println("Possible to play " + sets.size() + " sets (all players played unique games)");
+            System.out.println("Possible to play " + foundSets.size() + " sets (all players played unique games)");
 
-            // this filters out duplicate matches, though we still need a way to optimally arrange tem
-            final Set<Match> duplicates = new HashSet<>();
-            for (final Set<Match> match : sets) {
-                for (final Match match1 : match) {
-                    if (!duplicates.contains(match1)) {
-                        if (duplicates.stream().noneMatch(m -> m.isMirroredMatch(match1))) {
-                            duplicates.add(match1);
-                        } else {
-                            System.out.println("\tmirrored match: " + match1);
-                        }
-                    } else {
-                        System.out.println("\tdup for: " + match1);
-                    }
-                }
+            // filter out duplicate matches, though we still need a way to optimally arrange themm
+            final Set<Set<Match>> validSets = filterValidMatches(foundSets);
+
+
+
+            for (Set<Match> validSet : validSets) {
+                validSet.forEach(System.out::println);
+                System.out.println("---");
             }
-            System.out.println("======");
-            System.out.println("ALL UNIQUE MATCHES:  " + duplicates.size());
         }
 
         System.out.println("Took: " + (System.currentTimeMillis() - currentTime) + "ms");
+    }
+
+    private static Set<Set<Match>> filterValidMatches(final Set<Set<Match>> foundSets) {
+        final Set<Set<Match>> validSets = new HashSet<>();
+        final Set<Match> selectedMatches = new HashSet<>();
+
+        // if a set has some duplicate, nest to chuck the whole set out (since the uniqueness is determined by set)
+        for (final Set<Match> matchSet : foundSets) {
+            boolean validSet = true;
+            for (final Match match1 : matchSet) {
+                if (selectedMatches.contains(match1) || selectedMatches.stream()
+                        .anyMatch(m -> m.isMirroredMatch(match1))) {
+                    validSet = false;
+                }
+            }
+
+            if (validSet) {
+                validSets.add(matchSet);
+                selectedMatches.addAll(matchSet);
+            }
+        }
+
+        System.out.println("======");
+        System.out.println("ALL UNIQUE SETS:  " + validSets.size());
+        System.out.println("======");
+
+        return validSets;
     }
 
     private static void tabbedPrint(final int i, final String m) {
         System.out.println(Strings.repeat("\t", i) + m);
     }
 
-    public static Set<Set<Match>> findPossibleMatches(final Set<Team> teams, final Set<Player> allPlayers, final Set<Match> matchSets, final int i) {
+    public static Set<Set<Match>> findMatchSets(
+            final Set<Team> teams, final Set<Player> allPlayers, final Set<Match> localMatchSets, final int i) {
         final Set<Set<Match>> allMatchSets = new LinkedHashSet<>();
 
         final Set<Player> usedPlayers = new HashSet<>(8);
-        if (!matchSets.isEmpty()) {
-            tabbedPrint(i, "Adding players from: " + matchSets.size() + " match(es)!");
-            usedPlayers.addAll(matchSets.stream()
+        if (!localMatchSets.isEmpty()) {
+            tabbedPrint(i, "Adding players from: " + localMatchSets.size() + " match(es)!");
+            usedPlayers.addAll(localMatchSets.stream()
                     .flatMap(m -> m.getPlayers().stream())
                     .collect(Collectors.toSet()));
         }
 
         if (usedPlayers.containsAll(allPlayers)) {
             tabbedPrint(i, "Built complete match set for all players!");
-            tabbedPrint(i, "\tSet:" + matchSets);
+            tabbedPrint(i, "\tSet:" + localMatchSets);
 
             // exit point, built a unique set
-            allMatchSets.add(matchSets);
+            allMatchSets.add(localMatchSets);
         } else {
             final Set<Team> teamsWithUnusedPlayers = teams.stream()
                     .filter(t -> usedPlayers.stream().noneMatch(t.getPlayers()::contains))
@@ -121,18 +143,16 @@ public class Game {
                         // register local match
                         teamMatches.add(match);
 
-                        final Set<Match> subMatches = new HashSet<>(matchSets);
+                        final Set<Match> subMatches = new HashSet<>(localMatchSets);
                         subMatches.add(match);
 
-                        allMatchSets.addAll(findPossibleMatches(teams, allPlayers, subMatches, i + 1));
+                        // find rest of the matches remaining for played matches
+                        allMatchSets.addAll(findMatchSets(teams, allPlayers, subMatches, i + 1));
                     }
                 }
             }
         }
 
-        System.out.println("end:" + matchSets.size());
-        System.out.println("end all:" + allMatchSets.size());
-        System.out.println("--------");
         return allMatchSets;
     }
 }
